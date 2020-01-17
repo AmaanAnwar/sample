9f731306f61f52d4dc2ca1c43cf07739848b5aa0
root <root@localhost.localdomain>
2020-01-17 17:35:45 -0600

    First Commit

---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+    }
+}
